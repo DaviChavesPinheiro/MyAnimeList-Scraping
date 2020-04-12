a17dc27ba4ae6ebefd65f0a841c7cf800c160107
Davi <davicpinheiro123@hotmail.com>
2020-04-12 11:02:40 -0300

    Headers: To avoid 403 Forbidden error, is necessary to implement a header holding what browser type you are using.

--- scraping.py
+++ scraping.py
@@ -1,11 +1,18 @@
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
 
-url = "https://myanimelist.net/topmanga.php?type=bypopularity"
+url = "https://unionleitor.top/perfil-manga/shingeki-no-kyojin"
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
 
-response = urlopen(url)
-html = response.read()
-
-soup = BeautifulSoup(html, "html.parser")
-popularMangas = list(map(lambda a: a.text, soup.select('a.fs14')))
-print(popularMangas)
+try:
+    req = Request(url, headers = headers)
+    response = urlopen(req)
+    html = response.read()
+    soup = BeautifulSoup(html, "html.parser")
+    print(soup.select_one('h2').text)
+except HTTPError as e:
+    print(e.status, e.reason)
+except URLError as e:
+    print(e.reason)
+# popularMangas = list(map(lambda a: a.text, soup.select('a.fs14')))
