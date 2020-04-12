2b336e7d3ff96cdae2a5f74f433e64ec0af4a1c5
Davi <davicpinheiro123@hotmail.com>
2020-04-12 11:26:18 -0300

    HTML Processing: Process html request(bytes) to String (utf-8 encoding) and replace all '\t \a'... for avoid beautifulsoup decoding problems

--- scraping.py
+++ scraping.py
@@ -5,10 +5,15 @@ from bs4 import BeautifulSoup
 url = "https://unionleitor.top/perfil-manga/shingeki-no-kyojin"
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
 
+def process_html(input):
+    return " ".join(input.split()).replace("> <", "><")
+
 try:
     req = Request(url, headers = headers)
     response = urlopen(req)
     html = response.read()
+    html = html.decode('utf-8') # Decode Bytes to String in utf-8 encoding
+    html = process_html(html)
     soup = BeautifulSoup(html, "html.parser")
     print(soup.select_one('h2').text)
 except HTTPError as e:
